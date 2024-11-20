afb535c9e28c0a384c434971d85f9b69d5460f87
nicky19-manullang <nickysimanullang123@gmail.com>
2024-11-20 13:42:00 +0700

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,18 +3,6 @@ import "../styles/MainContent.css";
 
 function MainContent() {
   return (
-
-    <div className="col-12 p-4">
-      <section className="d-flex align-items-center justify-content-between p-5 bg-white">
-        <div className="col-md-6">
-          <h1>Welcome to JOUEnglish</h1>
-          <p>
-          Let's Unlock Your English Potential with JOUEnglish!
-          </p>
-        </div>
-      </section>
-    </div>
-==
     <main className="main-content">
       {/* Bagian Teks dan Logo */}
       <div className="content-wrapper">
@@ -30,4 +18,4 @@ function MainContent() {
   );
 }
 
-export default MainContent;
+export default MainContent;   
